1d9c68794c5aff97b37fda246e04c66358b41f77
ruesga-99 <jruesga411@gmail.com>
2025-02-03 10:37:27 -0600

    Added JS solution to Secret Friend Challenge (project_03)

--- project_03/app.js
+++ project_03/app.js
@@ -1,1 +1,67 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// List of registered friends
+let amigos = []
+
+function agregarAmigo () {
+    let texto = document.getElementById('amigo').value;
+
+    // Make sure the textbox isn't empty
+    if (!validarEntrada(texto)) {
+        alert('Por favor inserte un nombre.');
+        return;
+    }
+
+    // Add new friend to the list and HTML
+    amigos.push(texto);
+    actualizarLista(amigos);
+    limpiarCaja();
+}
+
+function sortearAmigo () {
+    // Make sure there's at least one friend in the list
+    if (amigos.length === 0) {
+        alert('Debes añadir al menos un elemento a la lista.')
+        return;
+    }
+
+    // Get a random friend and display it on the HTML site
+    numeroAleatorio = generarNumeroAleatorio();
+    amigoSecreto = amigos[numeroAleatorio];
+    console.log(numeroAleatorio, amigoSecreto);
+    document.getElementById('resultado').innerHTML = `<li>${amigoSecreto}</li>`;
+}
+
+function generarNumeroAleatorio() {
+    // Get a random number between 0 and amigos.lenght
+    return Math.floor(Math.random()*amigos.length);
+}
+
+function actualizarLista (amigos) {
+    // Clear current list to avoid duplicates
+    limpiarLista();
+    lista = document.getElementById('listaAmigos');
+
+    // Iterate over the friends list and show each element into a HTML list
+    for (i = 0; i < amigos.length; i++) {
+        let registro = document.createElement('li');
+        registro.textContent = amigos[i];
+        lista.appendChild(registro);
+    }
+}
+
+function validarEntrada (texto) {
+    // Make sure the textbox isn't empty
+    if (texto == null || texto == "") {
+        return false;
+    }
+    return true;
+}
+
+function limpiarCaja () {
+    // Clear textbox
+    document.getElementById('amigo').value = '';
+}
+
+function limpiarLista () {
+    // Clear HTML friends list
+    document.getElementById('listaAmigos').innerHTML = '';
+}
